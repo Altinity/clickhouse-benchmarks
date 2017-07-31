c4d0c039178c45ff2983fa96edcd99e80ee80779
sunsingerus <sunsingerus@gmail.com>
2017-07-31 17:54:01 +0300

    create download.sh and import.sh

--- airline/data/download.sh
+++ airline/data/download.sh
@@ -1,9 +1,19 @@
-#download database
+#!/bin/bash
 
-for s in `seq 1987 2017`
-do
-for m in `seq 1 12`
-do
-wget http://transtats.bts.gov/PREZIP/On_Time_On_Time_Performance_${s}_${m}.zip
+# download database
+
+ZIP_FILES_DIR="zip"
+mkdir "$ZIP_FILES_DIR"
+
+if [ ! -d "$ZIP_FILES_DIR" ]; then
+	"Can' use dir: $ZIP_FILES_DIR - not available"
+	exit 1
+fi
+
+for year in `seq 1987 2017`; do
+	for month in `seq 1 12`; do
+		FILE_NAME="On_Time_On_Time_Performance_${year}_${month}.zip"
+		wget -O "$ZIP_FILES_DIR/$FILE_NAME" "http://transtats.bts.gov/PREZIP/$FILE_NAME"
+	done
 done
-done
+
